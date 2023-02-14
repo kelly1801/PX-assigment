e805bc0a43d16bd4571fb1c25220a330be51bcec
kellyc <fabianachara@gmail.com>
2023-02-14 23:16:16 +0000

    Add styles for input bar search

--- src/styles/BarStyles.ts
+++ src/styles/BarStyles.ts
@@ -10,15 +10,19 @@ export const BarContainer = styled(Box)`
   form {
     display: flex;
     align-items: center;
-    justify-content: space-between input {
-      border: none;
-      outline: none;
-      width: 100%;
-    }
-    button {
-      background: none;
-      border: none;
-    }
+    justify-content: space-between;
+    
+  }
+
+  input {
+    border: none;
+    outline: none;
+    width: 100%;
+  }
+  
+  button {
+    background: none;
+    border: none;
   }
 
   @media only screen and (max-width: 834px) {
